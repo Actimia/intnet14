58665abcaa47c6971ca61f47ff902d482f41e194
Fredrik Hallberg <fhallb@kth.se>
2014-02-02 21:19:07 +0100

    Added message sending and command handling.

--- src/se/edstrompartners/intnet14/lab1/ChatClient.java
+++ src/se/edstrompartners/intnet14/lab1/ChatClient.java
@@ -24,116 +24,128 @@ import se.edstrompartners.net.gui.MainWindow;
  */
 public class ChatClient implements CommandListener {
 
-    private static final Pattern WHISPER = Pattern.compile("/(?:w|pm) ([\\w]+) ([\\S ]+)");
+	private static final Pattern WHISPER = Pattern.compile("/(?:w|pm) ([\\w]+) ([\\S ]+)");
 
-    private Network net;
-    private String name;
-    private MainWindow window;
-    public static void main(String[] args) {
-        try {
-            String host;
-            int port;
-            String name;
-            if (args.length == 0) {
-                host = "localhost";
-                port = 8080;
-                Random rnd = new Random();
-                name = "Guest_" + rnd.nextInt(100);
+	private Network net;
+	private String name;
+	private MainWindow window;
+	public static void main(String[] args) {
+		try {
+			String host;
+			int port;
+			String name;
+			if (args.length == 0) {
+				host = "localhost";
+				port = 8080;
+				Random rnd = new Random();
+				name = "Guest_" + rnd.nextInt(100);
 
-            } else {
-                host = args[0];
-                port = Integer.parseInt(args[1]);
-                name = args[2];
-            }
-            new ChatClient(InetAddress.getByName(host), port, name).start();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
+			} else {
+				host = args[0];
+				port = Integer.parseInt(args[1]);
+				name = args[2];
+			}
+			new ChatClient(InetAddress.getByName(host), port, name).start();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	public Network getNetwork(){
+		return net;
+	}
+	public ChatClient(InetAddress host, int port, String name) throws IOException {
+		net = new Network(this, new Socket(host, port));
+		window = new MainWindow(this,host+name);
+		this.name = name;
+	}
 
-    public ChatClient(InetAddress host, int port, String name) throws IOException {
-        net = new Network(this, new Socket(host, port));
-        window = new MainWindow(this,host+name);
-        this.name = name;
-    }
+	private void start() {
+		try (Scanner sc = new Scanner(System.in)) {
+			Thread t = new Thread(net);
+			t.start();
 
-    private void start() {
-        try (Scanner sc = new Scanner(System.in)) {
-            Thread t = new Thread(net);
-            t.start();
+			net.send(new Handshake(name));
+			while (true) {
+				// System.out.print("  ");
+				String line = sc.nextLine();
+				handleInput(line);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	public void handleInput(String line)  {
+		try {
+			if (line.startsWith("/")) {
+				// switch on first word of command
+				switch (line.substring(0,
+						line.indexOf(' ') == -1 ? line.length() : line.indexOf(' '))) {
+						case "/exit":
+							net.shutdown();
+							return;
+						case "/listusers":
+							net.send(new ListUsers());
+							break;
+						case "/help":
+							System.out.println("  These are the available commands:");
+							System.out.println("    /exit - shuts down the program.");
+							System.out.println("    /help - displays this information.");
+							System.out.println("    /listusers - lists all users in this chat.");
+							System.out.println("    /w NAME MESSAGE - send private message.");
+							System.out.println("    /pm NAME MESSAGE - synonym for /w.");
+							break;
+						case "/w":
+						case "/pm":
+							// use a regex to find the target and the message with groups
+							Matcher match = WHISPER.matcher(line);
+							if (match.matches() && match.groupCount() == 2) {
+								net.send(new PrivateMessage(name, match.group(1), match.group(2)));
+							} else {
+								System.out.println("  Private message syntax:\n    /w NAME MESSAGE");
+							}
+							break;
+						default:
+							System.out.println("  Unknown command, type /help for a list.");
+							break;
+				}
+			} else {
 
-            net.send(new Handshake(name));
-            while (true) {
-                // System.out.print("  ");
-                String line = sc.nextLine();
-                if (line.startsWith("/")) {
-                    // switch on first word of command
-                    switch (line.substring(0,
-                            line.indexOf(' ') == -1 ? line.length() : line.indexOf(' '))) {
-                    case "/exit":
-                        net.shutdown();
-                        return;
-                    case "/listusers":
-                        net.send(new ListUsers());
-                        break;
-                    case "/help":
-                        System.out.println("  These are the available commands:");
-                        System.out.println("    /exit - shuts down the program.");
-                        System.out.println("    /help - displays this information.");
-                        System.out.println("    /listusers - lists all users in this chat.");
-                        System.out.println("    /w NAME MESSAGE - send private message.");
-                        System.out.println("    /pm NAME MESSAGE - synonym for /w.");
-                        break;
-                    case "/w":
-                    case "/pm":
-                        // use a regex to find the target and the message with groups
-                        Matcher match = WHISPER.matcher(line);
-                        if (match.matches() && match.groupCount() == 2) {
-                            net.send(new PrivateMessage(name, match.group(1), match.group(2)));
-                        } else {
-                            System.out.println("  Private message syntax:\n    /w NAME MESSAGE");
-                        }
-                        break;
-                    default:
-                        System.out.println("  Unknown command, type /help for a list.");
-                        break;
-                    }
-                } else {
-                    net.send(new Message(name, line));
-                }
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
+				net.send(new Message(name, line));
 
-    @Override
-    public void onCommand(Network src, Command com) throws IOException {
-        switch (com.getType()) {
-        case MESSAGE:
-            Message msg = (Message) com;
-            if (msg.source.isEmpty()) {
-                System.out.println("  " + msg.message);
-                window.pushToWindow(msg.message);
-            } else {
-                System.out.println(msg.source + ": " + msg.message);
-                window.pushToWindow(msg.source + ": " + msg.message);
-            }
-            break;
-        case PRIVATEMESSAGE:
-            PrivateMessage pm = (PrivateMessage) com;
-            System.out.printf("  Private message from %s:%n    %s%n", pm.src, pm.msg);
-            if (!pm.tar.equals(name)) {
-                System.out.println("  Contact an admin. This pm was meant for " + pm.tar + ".");
-            }
-            break;
-        case NETWORKSHUTDOWN:
-            System.out.println("  Network connection lost.");
-            System.exit(0);
-            break;
-        default:
-            break;
-        }
-    }
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	public void onCommand(Network src, Command com) throws IOException {
+		switch (com.getType()) {
+		case MESSAGE:
+			Message msg = (Message) com;
+			if (msg.source.isEmpty()) {
+				System.out.println("  " + msg.message);
+				window.pushToWindow(msg.message);
+			} else {
+				System.out.println(msg.source + ": " + msg.message);
+				window.pushToWindow(msg.source + ": " + msg.message);
+			}
+			break;
+		case PRIVATEMESSAGE:
+			PrivateMessage pm = (PrivateMessage) com;
+			System.out.printf("  Private message from %s:%n    %s%n", pm.src, pm.msg);
+			if (!pm.tar.equals(name)) {
+				System.out.println("  Contact an admin. This pm was meant for " + pm.tar + ".");
+			}
+			break;
+		case NETWORKSHUTDOWN:
+			System.out.println("  Network connection lost.");
+			System.exit(0);
+			break;
+		default:
+			break;
+		}
+	}
 
 }
--- src/se/edstrompartners/net/gui/MainWindow.java
+++ src/se/edstrompartners/net/gui/MainWindow.java
@@ -3,6 +3,8 @@ package se.edstrompartners.net.gui;
 import java.awt.BorderLayout;
 import java.awt.Container;
 import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JFrame;
 import javax.swing.JScrollPane;
@@ -11,7 +13,7 @@ import javax.swing.JTextField;
 
 import se.edstrompartners.intnet14.lab1.ChatClient;
 
-public class MainWindow extends JFrame {
+public class MainWindow extends JFrame implements ActionListener {
 	private static final int width = 700;
 	private static final int height = 400;
 	
@@ -20,9 +22,9 @@ public class MainWindow extends JFrame {
 	private JTextArea jUsers;
 	private JTextArea jChat;
 	private JTextField jMessage;	
-
+	ChatClient cc;
 	public MainWindow(ChatClient chatClient, String windowTitle) {
-		
+		this.cc = chatClient;
 		pane = getContentPane();
 		
 		
@@ -31,8 +33,9 @@ public class MainWindow extends JFrame {
 		JScrollPane jsp2 = new JScrollPane(jChat);
 		
 		jMessage = new JTextField();
-		jMessage.setSize(700,30);
-		
+		//jMessage.setSize(700,30);
+		jMessage.addActionListener(this);
+		jMessage.setActionCommand("sendMessage");
 		pane.add(jsp2,BorderLayout.CENTER);
 		pane.add(jMessage,BorderLayout.PAGE_END);
 		
@@ -43,5 +46,13 @@ public class MainWindow extends JFrame {
 	}
 	public void pushToWindow(String text){
 		jChat.append(text + "\n");
+	}
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		switch (e.getActionCommand()){
+		case "sendMessage":
+			cc.handleInput(jMessage.getText());
+			jMessage.setText("");
+		}
 	}
 }
